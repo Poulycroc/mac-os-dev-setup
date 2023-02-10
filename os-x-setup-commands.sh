a4d19dbe270e080bb5f7663f791983650349ddec
Poulycroc <esga.retard@gmail.com>
2023-02-10 16:33:29 +0100

    Update os-x-setup-commands.sh

--- os-x-setup-commands.sh
+++ os-x-setup-commands.sh
@@ -110,6 +110,7 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 
 # NV-Chad NeoVim IDE
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
+# :TSInstall css html scss javascript php ruby go
 
 # NVM
 echo "source $(brew --prefix nvm)/nvm.sh" >> ~/.zshrc
